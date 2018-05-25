69af9f7769efa614bf3e31cc38c53b325aa2ef0f
takayamazusa <azusa.takayama@j-tech.jp>
2018-05-25 11:55:20 +0900

    コメントの追加と変数・関数名を変更

--- ConsoleApplication13/ConsoleApplication13.cpp
+++ ConsoleApplication13/ConsoleApplication13.cpp
@@ -5,25 +5,25 @@
 #include "stdio.h"
 #include"stdlib.h"
 
-static int all;
-static int chose;
+static int all; //抽選に参加する人数
+static int winner; //当選する人数
 
-int input();
-int input2();
-void lottery(int all, int chose);
+int AllNum();
+int WinnerNum();
+void lottery(int all, int winner);
 
 int main()
 {
-	all = input();
-	chose = input2();
+	all = AllNum();
+	winner = WinnerNum();
 
-	lottery(all, chose);
+	lottery(all, winner);
 
     return 0;
 }
 
 //全体の数を入力する
-int input() {
+int AllNum() {
 	printf("全体の数は何人ですか？\n");
 	scanf_s("%d", &all);
 	printf("%d人の中から", all);
@@ -32,17 +32,17 @@ int input() {
 }
 
 //抽選であたる数を入力する
-int input2() {
+int WinnerNum() {
 	printf("当たりは何人ですか？\n");
-	scanf_s("%d", &chose);
+	scanf_s("%d", &winner);
 	//printf("%d人\n", chose);
 
-	return chose;
+	return winner;
 }
 
 
 /*誰が当たるかだけ知りたい場合(すべて同じ景品の場合)*/
-/*void lottery(int all, int chose) {
+/*void lottery(int all, int winner) {
 	
 	int i;
 	int a[100] = { 0 };
@@ -52,11 +52,11 @@ int input2() {
 	for (i = 0; i < all; i++) {
 		a[i] = i + 1;
 		
-	}
+}
 	
 
 	//乱数を調べる
-	for (i = 0; i < chose; ) {
+	for (i = 0; i < winner; ) {
 
 		int num= rand() % all + 1;
 
@@ -75,20 +75,20 @@ int input2() {
 }*/
 
 /*順位をつけたい場合*/
-void lottery(int all, int chose) {
+void lottery(int all, int winner) {
 	int i = 0;
-	int win = 1; //当選者順位
+	int win = 1;                    //当選者順位を入れる変数
 
-	int a[100] = { 0 };
+	int a[100] = { 0 };             //0で初期化した大きな配列をつくる
 
-	for (i = 0; i < chose;i++ ) {
+	for (i = 0; i < winner;i++ ) {  //当選予定人数分、乱数であたりを出す
 
-		int num = rand() % all + 1;
+		int num = rand() % all + 1; //当選者の番号を乱数で決定、乱数を入れる変数をnumとする
 
-		if (a[num - 1] == 0) {
-			a[num - 1] = num;
+		if (a[num - 1] == 0) {      //乱数のひとつ小さい数字の配列の箇所に0が入っている場合
+			a[num - 1] = num;       //その値を格納する
 			printf("%d等　%d番\n", win, num);
-			win += 1;
+			win += 1;               //当選順位をカウントアップ
 		}
 	}
 }
